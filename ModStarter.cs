ef4eb068dd7643ccf7bcc3a068bb68660a3f548b
Carlos Alloatti <calloatti@gmail.com>
2026-04-17 07:53:56 -0300

    Add project files.

---
+++ ModStarter.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+using UnityEngine;
+
+namespace Calloatti.MapSizeTweaks
+{
+  public class ModStarter : IModStarter
+  {
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+
+      new Harmony("Calloatti.MapSizeTweaks").PatchAll();
+
+      Debug.Log("[MapSizeTweaks] Harmony Patches Applied.");
+    }
+  }
+}
